8ee66ea44e2919b2d20722cd33ce07ad3d33f7bf
yazdancode <yshabanei@gmail.com>
2025-08-26 14:40:29 +0330

    create profile.php

---
+++ template/profile.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="<?= assets('css/style.css') ?>">
+</head>
+<body>
+    <h1 style="text-align: center;">User Profile</h1>
+</body>
+</html>
