a82d69929a36ca721b32d0cdb8ae6ebcc2566704
yazdancode <yshabanei@gmail.com>
2025-08-26 15:28:09 +0330

    create page 404 and remove comment index.php

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
-// لود تنظیمات و هلسپرها
 require __DIR__ . '/config/init.php';
-
-// گرفتن مسیر URL
 $url = $_GET['url'] ?? 'login';
 $url = trim($url, '/');
-
-// مسیر فایل template
 $file = __DIR__ . "/template/{$url}.php";
 
 if (file_exists($file)) {
     require $file;
 } else {
-    echo "صفحه پیدا نشد!";
+    require __DIR__ . '/template/not-found.php';
 }
---
+++ template/not-found.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= assets('css/style_not_found.css') ?>">
+    <title>404</title>
+</head>
+<body>
+    <div class="content">
+        <h1>404</h1>
+        <h2>اوه، صفحه پیدا نشد!</h2>
+        <p>صفحه‌ای که به دنبال آن هستید یافت نشد (ممکن است منتقل شده، حذف شده یا اصلاً وجود نداشته باشد). متأسفیم بابت این مشکل.</p>
+        <p>اگر فکر می‌کنید این یک خطا است یا چیزی خراب شده، لطفاً گزارش دهید.</p>
+        <div class="btns">
+            <a href="<?= BASE_URL ?>login" class="fill">Go back</a>
+            <a href="#" class="">Report</a>
+        </div>
+    </div>
+</body>
+</html>
