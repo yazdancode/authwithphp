93fa9923af2c260f5b20400fb0d1fd4a4a59020f
yazdancode <yshabanei@gmail.com>
2025-08-26 14:39:03 +0330

    loading fix localhst

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
-</head>
-<body>
-    <h1 style="text-align: center;">User Profile</h1>
-</body>
-</html>
+<?php
+// لود تنظیمات و هلسپرها
+require __DIR__ . '/config/init.php';
+
+// گرفتن مسیر URL
+$url = $_GET['url'] ?? 'login';
+$url = trim($url, '/');
+
+// مسیر فایل template
+$file = __DIR__ . "/template/{$url}.php";
+
+if (file_exists($file)) {
+    require $file;
+} else {
+    echo "صفحه پیدا نشد!";
+}
--- template/register.php
+++ template/register.php
@@ -1,7 +1,3 @@
-<?php
-require __DIR__ . '/../config/init.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="rtl">
 
