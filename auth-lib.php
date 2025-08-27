e8a04e62976782a4ca4236fd2dddaa3a73faca74
yazdancode <yshabanei@gmail.com>
2025-08-27 12:56:22 +0330

    i added to function userExists to file auth-lib.php

--- auth-lib.php
+++ auth-lib.php
@@ -1,15 +1,16 @@
 <?php
+if (!function_exists('userExists')) {
+    function userExists(string $email, string $phone): bool
+    {
+        global $pdo;
 
-function userExists(string $email, string $phone): bool
-{
-    global $pdo;
+        $sql = "SELECT id FROM users WHERE email = :email OR phone = :phone LIMIT 1";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':email' => $email,
+            ':phone' => $phone
+        ]);
 
-    $sql = "SELECT id FROM users WHERE email = :email OR phone = :phone LIMIT 1";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        ':email' => $email,
-        ':phone' => $phone
-    ]);
-
-    return $stmt->fetchColumn() !== false;
+        return $stmt->fetchColumn() !== false;
+    }
 }
